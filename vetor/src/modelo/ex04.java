df7336980f4e649ab6d814aca537d3b6695fa782
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-08-28 12:07:52 -0300

    Update ex04.java

--- vetor/src/modelo/ex04.java
+++ vetor/src/modelo/ex04.java
@@ -1,3 +1,7 @@
+/*Escreva um algoritmo que leia dois vetores de
+10 posições e faça a soma dos elementos de
+mesmo índice, colocando o resultado em um
+terceiro vetor. Mostre o vetor resultante.*/
 package modelo;
 
 import java.util.Scanner;
