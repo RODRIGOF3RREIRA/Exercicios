4bae36c9ed2aa5ffde8d87ca696f542b69b6bd2a
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-09-06 20:08:30 -0300

    Update ex02.java

--- vetor/src/modelo/ex02.java
+++ vetor/src/modelo/ex02.java
@@ -4,6 +4,33 @@ escreva somente os números positivos que se
 encontram no vetor.*/
 package modelo;
 
+import java.util.Scanner;
+
 public class ex02 {
-    
-}
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in, "ISO-8859-1");
+
+        int i;
+        int vetor[] = new int[15];
+        int positivo[] = new int[15];
+
+        System.out.println("Digite 15 valores do vetor");
+        for (i = 0; i < 15; i++) {
+            vetor[i] = leitor.nextInt();
+        }
+
+        for (i = 0; i < 15; i++) {
+            if (vetor[i] >= 1) {
+                positivo[i] = vetor[i];
+            }else;
+        }
+        for (i = 0; i < 15; i++) {
+            System.out.println("Positivos: "+positivo[i]);
+        }
+        
+
+        }
+
+    }
+
