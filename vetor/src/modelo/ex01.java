402a8e44b949efc78d3a6d1241edb71e809765ba
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-09-06 19:45:35 -0300

    Update ex01.java

--- vetor/src/modelo/ex01.java
+++ vetor/src/modelo/ex01.java
@@ -3,6 +3,27 @@ números inteiros. Em seguida, calcule e escreva
 o somatório dos valores deste vetor.*/
 package modelo;
 
+import java.util.Scanner;
+
 public class ex01 {
+     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in, "ISO-8859-1");
+        
+    int i;
+    int soma = 0;
+    int numero[] = new int [10];
     
+    System.out.println("Digite 10 valores do vetor");
+    for(i = 0; i < 10; i++){
+        numero[i] = leitor.nextInt();
+    }
+    
+    for (i = 0; i < 10; i++){
+        soma = soma + numero[i];
+    }
+    
+    
+    System.out.println("A soma dos valores é: "+soma);
+    
+  }
 }
