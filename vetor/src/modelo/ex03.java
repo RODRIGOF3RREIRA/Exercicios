9d552d683ea088a07d00859d41952c5f541d3103
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-09-06 22:23:01 -0300

    Update ex03.java

--- vetor/src/modelo/ex03.java
+++ vetor/src/modelo/ex03.java
@@ -8,6 +8,34 @@ informando “O número não se encontra no
 vetor”.*/
 package modelo;
 
+import java.util.Scanner;
+
 public class ex03 {
-    
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in, "ISO-8859-1");
+
+        int usuario;
+        int vetor[] = new int[8];
+        int i;
+
+        System.out.println("Digite os valores do vetor:");
+        for (i = 0; i < 8; i++) {
+            vetor[i] = leitor.nextInt();
+        }
+
+        System.out.println("Busque um valor no vetor");
+        usuario = leitor.nextInt();
+
+        for (i = 0; i < 8; i++) {
+            if (usuario == vetor[i]) {
+                System.out.println("O vetor possui este valor");
+            }else {
+                System.out.println("O vetor não possui este valor");
+            }
+            break;
+
+        }
+
+    }
 }
