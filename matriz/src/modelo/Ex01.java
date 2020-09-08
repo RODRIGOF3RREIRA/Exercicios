6e8a6d8cca332f278c9e87791135d1ba75e1a8d2
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-09-08 10:21:47 -0300

    Update Ex01.java

--- matriz/src/modelo/Ex01.java
+++ matriz/src/modelo/Ex01.java
@@ -5,21 +5,30 @@ package modelo;
 import java.util.Scanner;
 
 public class Ex01 {
+
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in, "ISO-8859-1");
-        
-        int matriz[] [] = new int[5][5];
-        int i;
-        
+
+        int matriz[][] = new int[5][5];
+
         System.out.println("Digite os valores da matriz:");
-        for (i = 0; i < 25; i++) {
-            matriz[i] [i] = leitor.nextInt();
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("[%d][%d]: ", i + 1, j + 1);
+                matriz[i][j] = leitor.nextInt();
+            }
         }
         
-        for(i = 0; i < 25 ;i++){
-            System.out.println(matriz[i] [i]);
+        System.out.println("\nA Matriz ficou: \n");
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.printf("\t %d \t",matriz[i][j]); 
+            }
+            System.out.println();
         }
-        
+
     }
-    
+
 }
