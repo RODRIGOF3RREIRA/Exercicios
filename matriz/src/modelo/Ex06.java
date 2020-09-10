538487af0955d6eac25fc6b0ae208f5a085e19f0
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-09-09 23:35:09 -0300

    Update Ex06.java

--- matriz/src/modelo/Ex06.java
+++ matriz/src/modelo/Ex06.java
@@ -2,9 +2,50 @@
 uma terceira matriz tembém 2x5 com o valor da soma dos elementos de mesmo índice.*/
 package modelo;
 
+import java.util.Scanner;
 
 public class Ex06 {
+
     public static void main(String[] args) {
- 
+        Scanner leitor = new Scanner(System.in, "ISO-8859-1");
+
+        int matriz1[][] = new int[2][5];
+        int matriz2[][] = new int[2][5];
+        int matrizSoma[][] = new int[2][5];
+
+        System.out.println("Digite os valores da matriz 1:");
+
+        for (int i = 0; i < 2; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("[%d][%d]: ", i + 1, j + 1);
+                matriz1[i][j] = leitor.nextInt();
+            }
+        }
+
+        System.out.println("Digite os valores da matriz 2:");
+
+        for (int i = 0; i < 2; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("[%d][%d]: ", i + 1, j + 1);
+                matriz2[i][j] = leitor.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                matrizSoma[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+        
+        System.out.println("\nA Resultado: \n");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("\t %d \t", matrizSoma[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
