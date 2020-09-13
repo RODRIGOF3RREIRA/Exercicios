6ed0259e9496b59c7f44687b790315cf4d980de3
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2020-09-13 20:48:51 -0300

    update

--- listas/src/modelo/Lista_enc_n_ordenada.java
+++ listas/src/modelo/Lista_enc_n_ordenada.java
@@ -8,108 +8,168 @@ public class Lista_enc_n_ordenada {
 
         public int num;
         public LISTA prox;
+        private Object ant;
     }
+        //definindo a classe que representará
+        //cada elemento da lista
+    
 
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in, "ISO-8859-1");
-        //a lista esta vazia, logo o objeto inicio tem o valor null.
-        //objeto contem endereço do primeiro elemento da LISTA.
+        Scanner entrada = new Scanner(System.in);
+        //a lista está vazia,logo,
+        //o objeto inicio tem o valor null
+        //o objeto inicio conterá o endereço do primeiro elemento da lista
         LISTA inicio = null;
-        //objeto fim conterá endereço do ultimo elemento da lista.
+        // o objeto fim conterá o endereço do último elemento da lista
         LISTA fim = null;
-        //objeto anterior é objeto auxiliar 
+        // o objeto aux é um objeto auxiliar
+        LISTA aux;
+        // o objeto anterior é um objeto auxiliar
         LISTA anterior;
-
+        // apresentado o menu de opções
         int op, numero, achou;
-
         do {
-            System.out.println("Menu de opções");
-            System.out.println("1 - Inserir no inicio da lista");
+            System.out.println("\nMENU DE OPÇÕES\n");
+            System.out.println("1 - Inserir no início da lista");
             System.out.println("2 - Inserir no fim da lista");
-            System.out.println("3 - consultar toda a lista");
-            System.out.println("4 - Remover a lista");
-            System.out.println("5 - Esvaziar a lista");
-            System.out.println("6 -Sair");
-            System.out.println("Digite sua opção");
-            op = leitor.nextInt();
-            if (op < 1 || op > 6) {
-                System.out.println("Opção Invalida");
+            System.out.println("3 - Consultar a lista do inicio ao fim lista");
+            System.out.println("4 - Consultar a lista do fim ao inicio lista");
+            System.out.println("5 - Remover da lista");
+            System.out.println("6 - Esvaziar a lista");
+            System.out.println("7 - Sair");
+            System.out.print("Digite sua opção:");
+            op = entrada.nextInt();
+            if (op < 1 || op > 7) {
+                System.out.println("Opção inválida!!");
+            }
 
-                if (op == 1) {
-                    System.out.println("Digite o numero a ser inserido no inicio da lista");
-                    LISTA novo = new LISTA();
-                    novo.num = leitor.nextInt();
-                    if (inicio == null) {
-                        //a lista estava vazia e o elemento inserido será primeiro e ultimo
-                        inicio = novo;
-                        fim = novo;
-                        novo.prox = null;
-                    } else;
-                    //lista contem elementos
-                    //novo elemento será inserido no inicio da lista
-                    novo.prox = inicio;
+            if (op == 1) {
+                System.out.println("digite o número a ser inserido no início da lista:");
+				LISTA novo = new LISTA();
+                novo.num = entrada.nextInt();
+                if (inicio == null) {
+                    //a lista estava vazia e o elemento inserido será o primeiro e ultimo
                     inicio = novo;
-                }
-                System.out.println("Número inserido no inicio da lista");
-            }
-            if (op == 2){
-                System.out.println("Digite o número a ser inserido no fim da lista");
-                LISTA novo = new LISTA();
-                novo.num = leitor.nextInt();
-                if(inicio == null){
-                    //lista vazia
-                    //O elemento inserido é o inicio e o fim da lista
-                    inicio = novo;
-                    novo.prox = null;
-                }else{
-                    //lista ja contem algum elemento
-                    //o novo elemento sera inserido no fim da lista.
-                    fim.prox = novo;
                     fim = novo;
-                    fim.prox = null;
+                    novo.prox = null;
+                    novo.ant = null;
+                } else {
+                    //a lista já contem elementos e sera inserido no inicio da lista
+                    novo.prox = inicio;
+                    inicio.ant = inicio;
+                    novo.ant = null;
+                    inicio = novo;
                 }
-                System.out.println("Numero inserido no fim da lista");
+                System.out.println("numero inserido no inicio da lista");
             }
-            if(op == 3){
-                System.out.println("Lista vazia");
-            }else{
-                System.out.println("\n Consultando a lista");
-                LISTA aux = inicio;
-                while (aux != null) {
-                    System.out.println(aux.num+ " ");
-                    aux = aux.prox; 
-                    
+            if (op == 2) {
+                System.out.println("digite numero a ser inserido no fim da lista");
+                LISTA novo = new LISTA();
+                novo.num = entrada.nextInt();
+                if (inicio == null) {
+                    // a lista estava vazia
+                    //numero inserido sera o primeiro e ultimo
+                    inicio = novo;
+                    fim = novo;
+                    novo.prox = null;
+                    novo.ant = null;
+
+                } else {
+                    // a lista já contem elementos será inserido no fim da lista
+                    fim.prox = novo;
+                    novo.ant = fim;
+                    novo.prox = null;
+                    fim = novo;
+                }
+                System.out.println("numero inserido no fim da lista");
+            }
+
+            if (op == 3) {
+                if (inicio == null) {
+                    //a lista está vazia
+                    System.out.println("Lista vazia!!");
+                } else {
+                    // a lista contem elementos e o elemtos sera mostrado do inicio ao fim
+                    System.out.println("consultando a lista do inicio ao fim");
+                    aux = inicio;
+
+                    while (aux != null) {
+                        System.out.println(aux.num + " ");
+                        aux = aux.prox;
+                    }
                 }
             }
-            
-            if (op == 4){
-                if(inicio == null){
-                    System.out.println("Lista vazia");
-                }else{
-                    System.out.println("\n digite o elemento a ser removido");
-                    numero = leitor.nextInt();
-                    LISTA aux = inicio;
-                    anterior = null;
+
+            if (op == 4) {
+                if (inicio == null) {
+                    // a lista está vazia
+                    System.out.println("Lista vazia!!");
+                } else {
+                    // a lista contem elementos e estes serão mostrados do fim ao inicio
+                    System.out.println("\nConsultando a lista do fim ao inicio\n");
+                    aux = fim;
+                    while (aux != null) {
+                        System.out.print(aux.num + "  ");
+                        aux = aux.ant;
+                    }
+                }
+            }
+            if (op == 5) {
+                if (inicio == null) {
+                    // a lista está vazia
+                    System.out.println("Lista vazia!!");
+                } else {
+                    // a lista contem elementos e o elemento a ser removido deve ser digitado
+                    System.out.println("\nDigite o elemento a ser removido: ");
+                    numero = entrada.nextInt();
+                    // todas as ocorrencias da lista iguais ao número digitado serão removidas
+                    aux = inicio;
                     achou = 0;
-                    while(aux != null){
-                        if(aux.num == numero){
-                            achou = + 1;
-                            if(aux == inicio){
-                                //o numero a ser removido é o primeiro da lista
+                    while (aux != null) {
+                        if (aux.num == numero) {
+                            // o numero digitado foi encontrado na lista e sera removido
+                            achou = achou + 1;
+                            if (aux == inicio) {
+                                // o numero a ser removido é o primeiro da lista
                                 inicio = aux.prox;
+                                if (inicio != null) {
+                                    inicio.ant = null;
+                                }
                                 aux = inicio;
-                            }else if (aux == fim){
-                                anterior.prox = null;
-                                fim = anterior;
+                            } else if (aux == fim) {
+                                // o numero a ser removido é o ultimo da lista
+                                fim = (LISTA) fim.ant;
+                                fim.prox = null;
                                 aux = null;
+                            } else {
+                                // o numero a ser removido esta no meio da lista
+                                aux.ant = aux.prox;
+                                aux = aux.prox;
                             }
+                        } else {
+
+                            aux = aux.prox;
                         }
                     }
-                    
+                    if (achou == 0) {
+                        System.out.println("Número nao encontrado");
+                    } else if (achou == 1) {
+                        System.out.println("numero removido 1 vez");
+                    } else {
+                        System.out.println("número removido " + achou + " vezes");
+                    }
                 }
             }
-            
-        }while(op != 5);
+            if (op == 6) {
+                if (inicio == null) {
+                    // a lista está vazia
+                    System.out.println("Lista vazia!!");
+                } else {
+                    // a lista será esvaziada
+                    inicio = null;
+                    System.out.println("Lista esvaziada");
+                }
+            }
+        } while (op != 7);
     }
 }
-
--- listas/src/modelo/Lista_enc_ordenada.java
+++ listas/src/modelo/Lista_enc_ordenada.java
@@ -1,7 +1,147 @@
-
 package modelo;
 
+import java.util.Scanner;
 
 public class Lista_enc_ordenada {
-    
+
+    //definindo a classe que representa cada elemento da lista
+    private static class LISTA {
+
+        public int num;
+        public LISTA prox;
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //lista esta vazia logo inicio tem valor nulo
+        //objeto inicio primeiro elemento da lista
+        LISTA inicio = null;
+        //objeto fim é final da lista
+        LISTA fim = null;
+        //objeto aux é objeto auxiliar
+        LISTA aux;
+        //objeto anterior é objeto auxiliar
+        LISTA anterior;
+        // apresentar menu de opções
+        int op, numero, achou;
+        do {
+            System.out.println("\n MENU DE OPÇÕES\n");
+            System.out.println(" 1 - inserir no inicio da lista");
+            System.out.println(" 2 - consultar toda a lista");
+            System.out.println(" 3 - remover da lista");
+            System.out.println(" 4 - esvaziar a lista");
+            System.out.println(" 5 - sair");
+            System.out.println(" digite sua opção");
+            op = entrada.nextInt();
+            if (op < 1 || op > 5) {
+                System.out.println("opção invalida");
+            }
+            if (op == 1) {
+                System.out.println(" digite numero a ser inserido na lista");
+                LISTA novo = new LISTA();
+                novo.num = entrada.nextInt();
+                if (inicio == null) {
+                    // a lista esta vazia e o valor inserido é o primeiro e ultimo elemento da lista
+                    inicio = novo;
+                    fim = novo;
+                    novo.prox = null;
+                } else {
+                    //a lista ja contem elemento
+                    //o novo elemento sera inserido na ordem crescente
+                    anterior = null;
+                    aux = inicio;
+                    while (aux != null && novo.num > aux.num) {
+                        anterior = aux;
+                        aux = aux.prox;
+                    }
+                    if (anterior == null) {
+                        // o novo numero inserido e menor que todos da lista logo sera inserido no inicio
+                        novo.prox = inicio;
+                        inicio = novo;
+                    } else if (aux == null) {
+                        //novo lemento e maior que todos da lista e sera inserido no final
+                        fim.prox = novo;
+                        fim = novo;
+                        fim.prox = null;
+                    } else {
+                        //o novo numero sera inserido no meio da lista entre 2 numeros ja existente
+                        anterior.prox = novo;
+                        novo.prox = aux;
+                    }
+                }
+                System.out.println("numero inserido na lista");
+
+            }
+
+            if (op == 2) {
+                if (inicio == null) {
+                    //lista esta vazia
+                    System.out.println("A lista esta vazia");
+                } else {// a lista sera mostrada do inicio ao fim
+                    System.out.println("consultando a lista");
+                    aux = inicio;
+                    while (aux != null) {
+                        System.out.println(aux.num + " ");
+                        aux = aux.prox;
+                    }
+                }
+            }
+
+            if (op == 3) {
+                if (inicio == null) {
+                    //a lista esta vazia
+                    System.out.println("A lista esta vazia");
+                } else {
+                    // a lista contem elementos e o elemento digitado sera removido
+                    System.out.println("digite o elemento para remover");
+                    numero = entrada.nextInt();
+                    //remover todos elementos iguais
+                    aux = inicio;
+                    anterior = null;
+                    achou = 0;
+                    while (aux != null) {
+                        if (aux.num == numero) {
+                            // o numero digitado foi encontrado e sera removido
+                            achou = achou + 1;
+                            if (inicio == inicio) {
+                                // o numero a ser removido e o primeiro da lista
+                                inicio = aux.prox;
+                                aux = inicio;
+                            } else if (aux == fim) {
+                                // o elemento a ser removido é ultimo da lista
+                                anterior.prox = null;
+                                fim = anterior;
+                                aux = null;
+                            } else {
+                                // o elemento a ser removido esta no meio da lista
+                                anterior.prox = aux.prox;
+                                aux = aux.prox;
+                            }
+                        } else {
+                            LISTA anteiror = aux;
+                            aux = aux.prox;
+                        }
+                    }
+                    if (achou == 0) {
+                        System.out.println("numero nao encontrado");
+                    } else if (achou == 1) {
+                        System.out.println("numero removido 1 vez");
+                    } else {
+                        System.out.println("numero removido" + achou + "vezes");
+                    }
+                }
+            }
+            if (op == 4) {
+                if (inicio == null) {
+                    //lista esta vazia
+                    System.out.println("A lista esta vazia");
+                } else {
+                    //a lsita sera aesvaziada
+                    inicio = null;
+                    System.out.println("A lista esvaziada");
+                }
+            }
+        } while (op != 5);
+    }
+
 }
